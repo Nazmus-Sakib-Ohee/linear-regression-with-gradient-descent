0e9366c2c099a19e8e8cb2353d799fad8c82be24
Nazmus Sakib Ohee <33629806+Nazmus-Sakib-Ohee@users.noreply.github.com>
2019-10-22 21:34:18 +0600

    The final version

--- OheeLRWGDA.cpp
+++ OheeLRWGDA.cpp
@@ -2,22 +2,93 @@
 using namespace std;
 int main()
 {
+//Coded with explanations By Md.Nazmus Sakib Ohee (CSE 30002).
 
-double xValues[] = {1, 2, 4, 3, 5};
-double yValues[] = {1, 3, 3, 2, 5};
+//initializing the loop count variable.
+int loopFor;
 
+//initializing b0 variable with a value of 0.
 double b0Value = 0;
+
+//initializing b1 variable with a value of 0.
 double b1Value = 0;
-double learningRate = 0.05;
+
+//printing message for user to insert the number of loops for the x,y inputs
+cout<<"Enter the total number of inputs pair\n";
+
+//takes input for number of loops
+cin>>loopFor;
+cout<<"\n";
+
+//initializing the xValues variable with indexes up to loopFor.
+double xValues[loopFor];
+
+//initializing the yValues variable with indexes up to loopFor.
+double yValues[loopFor];
+
+
+//initializing learningRate variable.
+double learningRate;
+
+cout<<"Enter the learning rate\n";
+//takes input for learningRate
+cin>>learningRate;
+cout<<"\n";
+//loop start from index 0
+for(int j=0;j<loopFor;j++){
+
+    cout<<"\nEnter The value of x"<<j+1<<"\n";
+
+    //takes input for xValues
+    cin>>xValues[j];
+    cout<<"\n";
+
+    cout<<"Enter The value of y"<<j+1<<"\n";
+
+    //takes input for yValues
+    cin>>yValues[j];
+    cout<<"\n";
+}
+
+
 cout<<"\n\n";
-for (int i = 0; i < 30; i ++) {
-    int modValue = i % 5;
-    double p = b0Value + b1Value * xValues[modValue];
-    double errorCalculate = p - yValues[modValue];
-    b0Value = b0Value - learningRate * errorCalculate;
-    b1Value = b1Value - learningRate * errorCalculate * xValues[modValue];
-    cout<<"b0 Value ="<<b0Value<<"\n";
-    cout<<"b1 Value ="<<b1Value<<"\n";
+
+//number of loops for the prediction is loopPrediction
+
+//initializing loopPrediction variable.
+int loopPrediction;
+
+//takes input for loopPrediction
+cout<<"Enter the number of loop for prediction \n";
+cin>>loopPrediction;
+cout<<"\n";
+
+for (int i = 0; i < loopPrediction; i ++) {
+
+    //number of inputs available is equal to loopFor
+
+    //calculating Mod value
+    int modValue = i%loopFor;
+
+    //calculating hypothesis value
+    double hypo = b0Value + b1Value*xValues[modValue];
+
+     //calculating error
+    double errorCalculate = hypo-yValues[modValue];
+
+    //calculating b0 value
+    b0Value = b0Value-learningRate*errorCalculate;
+
+    //calculating b1 value
+    b1Value = b1Value-learningRate*errorCalculate*xValues[modValue];
+
+    //printing b0 value
+    cout<<"b0 Value at loop "<<i<<" is ="<<b0Value<<"\n";
+
+    //printing b1 value
+    cout<<"b1 Value at loop "<<i<<" is ="<<b1Value<<"\n";
+
+    //printing error value
     cout<<"Calculated Error ="<<errorCalculate<<"\n\n";
 }
 
